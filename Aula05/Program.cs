cf996afa60a29c802a63551243b9a8464142b3da
matheuscx1 <matheuscx1@hotmail.com>
2025-07-02 22:32:20 -0300

    Adicionando nova aula

---
+++ Aula05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-----CALCULADORA-----");
+            Console.WriteLine("Digite o primeiro número: ");
+            int input1 = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            int input2 = Convert.ToInt32 (Console.ReadLine());
+
+
+            int sum = input1 + input2;
+            int subtraction = input1 - input2;
+            int multiplication = input1 * input2;
+            int division = input1 / input2;
+            int modulo = input1 % input2;
+            Console.WriteLine("A soma é: " + sum);
+            Console.WriteLine("A subtração é: " + subtraction);
+            Console.WriteLine("A multiplicação é: " + multiplication);
+            Console.WriteLine("A divisão é: " + division);
+            Console.WriteLine("O módulo é: " + modulo);
+        }
+    }
+
+}
---
+++ Aula06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Aula06
+{     class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("====Verificador de numeros Pares ou Ímpares====");
+            Console.Write("Digite um número: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("O número {0} é Par.", number);
+            }
+            else
+            {
+                Console.WriteLine("O número {0} é Ímpar.", number);
+            }                                         
+
+        }
+    }
+}
