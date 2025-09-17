2b5c77f439bdfaf06397186e154f369e9747c758
Matheus Amorim <145023775+matheuscx1@users.noreply.github.com>
2025-09-17 10:41:14 -0300

    Atualizando Program.cs Aula02

---
+++ Aula02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Qual é o seu nome?");
+        string nome = Console.ReadLine();
+        
+        Console.WriteLine($"Prazer em te conhecer, {nome}!");
+    }
+}
